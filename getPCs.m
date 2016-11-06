2052620c8333faa990d61cdf0d0b219bdcf952a8
berneezy3 <bernardcwang@gmail.com>
2016-11-06 14:23:10 -0800

    Add files via upload
    add function that extracts PCs

---
+++ getPCs.m
@@ -0,0 +1,21 @@
+load('Kaneshiro_etAl_objectCategoryEEG/s1.mat');
+
+[U,S,V] = svd(X);
+
+%%
+% This graph shows how many PCs explain 
+diagS = diag(S);
+diagS = diagS.^2;
+diagS = diagS/sum(diagS);
+%find first variable in cumsumDiagS that is above explThresh, 
+%arbitrarily set explThresh to .9
+explThresh = .9;
+cumsumDiagS = cumsum(diagS);
+nPC = find(cumsumDiagS>=explThresh, 1);
+
+plot(cumsumDiagS);
+xlim([0,250]);
+
+xPC = X * V;
+xPCuse = xPC(:,1:nPC);
+
