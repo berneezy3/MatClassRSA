f77be1591a3d1f817dbccf78a4d24810e8393723
Bernard Wang <berneezy@DN0a182db6.SUNet>
2017-04-09 14:36:03 -0700

    changes to not dendrogram

---
+++ src/Classifiers/classifyEEGData.m
@@ -0,0 +1,75 @@
+function [predictions, accuracy, confusionMatrix] = classifyEEGData(X, Y, classifier, varargin)
+
+    classifier = char(classifier);
+    
+    tic
+    addpath('glmnet_matlab/');
+    addpath('libsvm-3.21/matlab');
+
+    %%%
+    %%% INPUT PARSING :)
+    %%%
+    
+    p = inputParser;
+    
+    p.addRequired('X');
+    p.addRequired('Y');
+    
+    %defaultClassifier = 'SVM';
+    validClassifiers = {'LDA', 'SVM','RandomForest', 'ElasticNet', 'Multinomial'};
+    checkClassifier = @(x) any(validatestring(x,validClassifiers));
+    p.addRequired('classifier', checkClassifier);
+    
+    checkVarPC = @(x) any(x<=1 && x>0);
+    p.addOptional('varPC', NaN,checkVarPC);
+    
+    checkNumPC = @(x) any(x>1);
+    p.addOptional('numPCs', NaN,checkNumPC);
+
+    p.KeepUnmatched = true;
+    parse(p, X, Y, classifier, varargin{:});
+    
+    inputs = p.Results
+    
+
+    if ~isnan(inputs.numPCs) && ~isnan(inputs.varPC)
+       warning('variance explained by PCs and number of PCs both set.  varPC will override numPCs');
+       X = getPCs(X, inputs.varPC);
+    elseif ~isnan(inputs.numPCs)
+        X = getPCs(X, inputs.numPCs);
+    elseif ~isnan(inputs.varPC)
+        X = getPCs(X, inputs.varPC);
+    end
+
+    %X = getPCs(X, .9);
+    disp(size(X));
+    
+    predictions = [];
+    acc = NaN;
+    testInd = [];
+    
+    switch classifier
+        case 'SVM'
+            disp('in svm')
+            [predictions accuracy testIndex] = SVM(X, Y, 'rbf');
+        case 'RandomForest'
+            disp('in RF')
+            [predictions accuracy testIndex] = RandomForest(X, Y, 350 );
+        case 'LDA'
+            disp('in LDA')
+            [predictions accuracy testIndex] = LDA(X, Y);
+        case 'ElasticNet'
+            disp('in ENET')
+            [predictions accuracy] = ElasticNet(X, Y);
+        case 'Multinomial'
+            disp('in multinomial')
+            [predictions accuracy testIndex] = Multinomial(X, Y);
+        otherwise
+            error('input valid kernel type')
+    end
+    
+    confusionMatrix = confusionmat(Y, predictions);
+    imagesc(confusionMatrix);
+    toc
+    
+end
