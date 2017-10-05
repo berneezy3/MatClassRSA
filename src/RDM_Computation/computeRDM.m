def79254b5fb40d22e742a49ed3223ee89188756
Bernard Wang <berneezy@Bernards-MacBook-Pro-2.local>
2017-10-04 20:26:39 -0700

    finishing docstrings

--- src/RDM_Computation/computeRDM.m
+++ src/RDM_Computation/computeRDM.m
@@ -1,9 +1,5 @@
 function RDM = computeRDM(CM, varargin)
 %-------------------------------------------------------------------
-% (c) Bernard Wang and Blair Kaneshiro, 2017.
-% Published under a GNU General Public License (GPL)
-% Contact: bernardcwang@gmail.com
-%-------------------------------------------------------------------
 % RDM = computeRDM(CM, varargin)
 % ------------------------------------------------
 % Blair - January 31, 2017
@@ -30,6 +26,37 @@ function RDM = computeRDM(CM, varargin)
 % - Computing ranks (with ties): tiedrank
 % - Computing inverse percentile: http://bit.ly/2koMsAn
 % - Harmonic mean with two numbers: https://en.wikipedia.org/wiki/Harmonic_mean#Two_numbers
+
+% This software is licensed under the 3-Clause BSD License (New BSD License), 
+% as follows:
+% -------------------------------------------------------------------------
+% Copyright 2017 Bernard C. Wang, Anthony M. Norcia, and Blair Kaneshiro
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, 
+% this list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright notice, 
+% this list of conditions and the following disclaimer in the documentation 
+% and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its 
+% contributors may be used to endorse or promote products derived from this 
+% software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ?AS IS?
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
 
 % Initialize the input parser
 ip = inputParser;
--- src/RDM_Computation/normalizeMatrix.m
+++ src/RDM_Computation/normalizeMatrix.m
@@ -1,9 +1,5 @@
 function xOut = normalizeMatrix(xIn, normType)
 %-------------------------------------------------------------------
-% (c) Bernard Wang and Blair Kaneshiro, 2017.
-% Published under a GNU General Public License (GPL)
-% Contact: bernardcwang@gmail.com
-%-------------------------------------------------------------------
 % xOut = normalizeMatrix(xIn, normType)
 % --------------------------------------------------------
 % Blair - February 22, 2017
@@ -22,6 +18,37 @@ function xOut = normalizeMatrix(xIn, normType)
 % above normType options are specified, the function returns an error. 
 % Currently if the divisor is zero, the function returns an error and it 
 % is up to the user to adjust the input matrix.
+
+% This software is licensed under the 3-Clause BSD License (New BSD License), 
+% as follows:
+% -------------------------------------------------------------------------
+% Copyright 2017 Bernard C. Wang, Anthony M. Norcia, and Blair Kaneshiro
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, 
+% this list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright notice, 
+% this list of conditions and the following disclaimer in the documentation 
+% and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its 
+% contributors may be used to endorse or promote products derived from this 
+% software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ?AS IS?
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
 
 normType = lower(normType);
 
