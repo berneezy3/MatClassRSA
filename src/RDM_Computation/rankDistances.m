5f3ef8d635657c6e1a068c71b0dd4a0eb2bb2f77
Blair Kaneshiro <blairbo@gmail.com>
2018-09-13 16:54:44 -0700

    Added more info to rankDistances.m docstring

--- src/RDM_Computation/rankDistances.m
+++ src/RDM_Computation/rankDistances.m
@@ -5,16 +5,31 @@ function xOut = rankDistances(xIn, rankType)
 % Blair - February 22, 2017
 %
 % This function ranks distances in a symmetric distance matrix.
+%
 % Inputs:
-% - xIn: The square confusion matrix, possibly normlized
+% - xIn: The square distance matrix. Note that this function operates over
+%   only the lower triangle of the matrix, so any symmetrization steps
+%   should already have been performed. 
 % - rankType
-%   - Rank ('rank', 'r')
-%   - Percent rank ('percentrank', 'p')
-%   - None ('none', 'n')
+%   - Rank ('rank', 'r'): This procedure ranks the elements of the lower
+%   triangle, adjusting for ties (uses tiedrank), and then copies those
+%   elements to the upper triangle to produce a symmetric distance matrix.
+%   - Percent rank ('percentrank', 'p'): This procedure computes percentile
+%   ranks by first computing the ranks of the elements of the lower
+%   triangle, adjusting for ties, and then dividing the output by the
+%   number of elements in the lower triangle. Note that by operating on the
+%   lower triangle only, the number of elements is the number of unique
+%   pairs across the set of classes.
+%   - None ('none', 'n'): No ranking is performed.
 %
 % There is no default rank type, as rank type is assumed to be decided
 % prior to calling this function. If none of the above rankType options
 % are specified, the function returns an error.
+%
+% Output:
+% - xOut: The ranked matrix (same size as the input, and symmetric whether
+%   or not the input matrix was symmetric, unless no ranking was 
+%   performed).
 
 % This software is licensed under the 3-Clause BSD License (New BSD License),
 % as follows:
