3bfa8cb2dd5b2985dc6111542f7e98f6dadc15d7
Blair Kaneshiro <blairbo@gmail.com>
2019-08-12 17:52:18 -0700

    updated license for shuffleData.m

--- src/Preprocessing/shuffleData.m
+++ src/Preprocessing/shuffleData.m
@@ -37,6 +37,38 @@ function [randX, randY, randP, randIdx] = shuffleData(X, Y, P, r)
 % - Confirm whether labels vector has to be numeric or can be cell array of
 %   strings.
 
+% This software is licensed under the 3-Clause BSD License (New BSD License),
+% as follows:
+% -------------------------------------------------------------------------
+% Copyright 2019 Bernard C. Wang, Nathan C. L. Kong, Anthony M. Norcia, 
+% and Blair Kaneshiro
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice,
+% this list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+% this list of conditions and the following disclaimer in the documentation
+% and/or other materials provided with the distribution.
+%
+% 3. Neither the name of the copyright holder nor the names of its
+% contributors may be used to endorse or promote products derived from this
+% software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ?AS IS?
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+
 % Set random number generator
 if nargin < 4 || isempty(r), rng('shuffle');
 elseif length(r) == 2, rng(r{1}, r{2});
