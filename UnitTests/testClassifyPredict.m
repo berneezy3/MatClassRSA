9873df39d4fd738e34a2a67c95019598a6584ff5
babylab <babylab@sr15-4f84dfb0d1.stanford.edu>
2019-08-30 09:39:58 -0700

    Added test case for pairwise and additional comments for other test case.

--- UnitTests/testClassifyPredict.m
+++ UnitTests/testClassifyPredict.m
@@ -14,10 +14,10 @@ load('S06.mat');
 X_2D = reshape(X, [dim1*dim2, dim3]);
 X_2D = X_2D';
 
-%% Classification and prediction with 2D data using LDA
-% Issue related to the random seed:
-%    - Although the confusion matrices are the same for both runs, the
-%      C.predY and C1.predY vectors are different.
+%% Classification and prediction with 2D data using LDA and random seed
+% Run successfully, predictions are the same in both models
+% Issue:
+%    - CM is no longer in the struct
 
 M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'LDA', 'PCA', 0, 'randomSeed', 1);
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3), 'randomSeed', 1);
@@ -30,7 +30,8 @@ figure; plotMatrix(C1.CM, 'matrixLabels', 0, 'colorMap', 'jet');
 %% Classification and prediction with 2D data using random forest
 % Runs successfully, and CM looks OK.
 % Issue:
-%    - Why is PCA being run here?
+%    - Error using classifyPredict (line 103)
+%      Expected a string for the parameter name, instead the input type was 'double'.
 
 M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'RF', 'numTrees', 200, 'minLeafSize', 1);
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3));
@@ -39,10 +40,11 @@ figure; plotMatrix(C.CM, 'matrixLabels', 0, 'colorMap', 'jet');
 
 %% Classification and prediction with 2D data using SVM
 % Issues:
-%    - Output CM does not make any sense
+%    - Output CM does not exist (non-existent field)
 %    - Already stated in testClassifyTrain.m, but even though {'PCA', 0} is
-%      set, PCA is being run.  However, if we do not input the kernel
-%      name-value pair, PCA is not run (which is expected).
+%      set, PCA is being run.
+%    - Error using classifyPredict (line 103)
+%      Expected a string for the parameter name, instead the input type was 'double'.
 
 M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'SVM', 'kernel', 'rbf', 'PCA', 0);
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3));
@@ -57,7 +59,11 @@ M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classif
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3));
 figure; plotMatrix(C.CM, 'matrixLabels', 0, 'colorMap', 'jet');
 
+%% Classification and prediction with 2D data using pairwise and LDA
+% Runs successfully
 
+M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'LDA', 'PCA', 0, 'randomSeed', 1, 'pairwise', 1);
+C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3), 'randomSeed', 1);
 
 
 
