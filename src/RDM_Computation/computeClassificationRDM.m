b58df9f27e4a0e844b0d33bf7dd5936b2111e20b
Blair Kaneshiro <blairbo@ccrma.stanford.edu>
2019-09-02 16:05:19 -0700

    edit disp msg computeClassificationRDM.m

--- src/RDM_Computation/computeClassificationRDM.m
+++ src/RDM_Computation/computeClassificationRDM.m
@@ -11,36 +11,36 @@ function [RDM, params] = computeClassificationRDM(M, matrixType, varargin)
 % Required inputs:
 % - M: A square confusion matrix or matrix of pairwise accuracies.
 % - matrixType: String specifying the type of input matrix.
-%   - Enter 'p', 'pair', 'pairs', or 'pairwise' if inputting a matrix of
+%   -- Enter 'p', 'pair', 'pairs', or 'pairwise' if inputting a matrix of
 %     pairwise accuracies (values between 0 and 1.0).
-%   - Enter 'm', 'multi', 'multiclass', or 'multicategory' if inputting a
+%   -- Enter 'm', 'multi', 'multiclass', or 'multicategory' if inputting a
 %     confusion matrix from multicategory classification (typically a
 %     matrix of counts).
 %
 % Optional name-value pairs:
 % - 'normalize': 'diagonal', 'sum', 'none'
-%   - For 'multiclass' input matrix M, the default is 'diagonal', but any
+%   -- For 'multiclass' input matrix M, the default is 'diagonal', but any
 %     of the three options may be specified.
-%   - For 'pairwise' input matrix M, the default and only specification is
+%   -- For 'pairwise' input matrix M, the default and only specification is
 %     'none'. If 'diagonal' or 'sum' is specified with this input type,
 %     the function will override it with 'none' and print a warning.
 % - 'symmetrize': 'arithmetic', 'geometric', 'harmonic', 'none'
-%   - For 'multiclass' input matrix M, the default is 'arithmetic', but any
+%   -- For 'multiclass' input matrix M, the default is 'arithmetic', but any
 %     of the four options may be specified.
-%   - For 'pairwise' input matrix M, the default and only specification is
+%   -- For 'pairwise' input matrix M, the default and only specification is
 %     'none'. Any of the four options may be specified, but should not
 %     affect an input matrix that is symmetric to begin with.
 % - 'distance': 'linear', 'power', 'logarithmic', 'none'
 % - 'distpower': Integer > 0 (if using 'power' or 'log' distance)
-%   - For 'multiclass' input matrix M, the default 'distance' is 'linear',
+%   -- For 'multiclass' input matrix M, the default 'distance' is 'linear',
 %     but any of the four options may be specified. The default 'distpower'
 %     is 1 and applies only to 'power' and 'logarithmic' specifications
 %     of 'distance'.
-%   - For 'pairwise' input matrix M, the default and only specification is
+%   -- For 'pairwise' input matrix M, the default and only specification is
 %     'none'. If any other specification is given, the function will
 %     override it with 'none' and print a warning.
 % - 'rankdistances': 'none' (default), 'rank', 'percentrank'
-%   - For all input matrices M, the default is 'none'. If 'rank' or
+%   -- For all input matrices M, the default is 'none'. If 'rank' or
 %     'percentrank' are called but the input matrix is not symmetric,
 %     the subfunction will issue a warning and operate only on the lower
 %     triangle of the matrix, returning a symmetric matrix.
@@ -49,8 +49,8 @@ function [RDM, params] = computeClassificationRDM(M, matrixType, varargin)
 % - RDM: The Representational Dissimilarity (distance) Matrix. RDM is a
 %   square matrix of the same size as the input variable CM.
 % - params: RDM computation parameters. It is a struct whose fields specify
-%   normalization, symmetrization, distance computation, and distance
-%   ranking.
+%   normalization, symmetrization, distance measure, distance power, and 
+%   ranking of distances.
 %
 % Notes
 % - Computing ranks (with ties): tiedrank
@@ -89,7 +89,10 @@ function [RDM, params] = computeClassificationRDM(M, matrixType, varargin)
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.
 
-%%%%%%%%%%%%%%%%%%%%%%% Input parser %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Begin input parser
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Initialize the input parser
 ip = inputParser;
 ip.CaseSensitive = false;
@@ -101,12 +104,12 @@ matrixType = lower(matrixType); % Convert to lowercase
 
 % Specify input parser parameters based in pairwise or multiclass
 if any(strcmp(matrixType, {'p', 'pair', 'pairs', 'pairwise'}))
-    disp('Doing pairwise')
+    disp('Operating on pairwise input matrix.')
     defaultNormalize = 'none';
     defaultSymmetrize = 'none';
     defaultDistance = 'none';
 elseif any(strcmp(matrixType, {'m', 'multi', 'multiclass', 'multicategory'}))
-    disp('Doing multiclass')
+    disp('Operating on multiclass input matrix.')
     defaultNormalize = 'diagonal';
     defaultSymmetrize = 'arithmetic';
     defaultDistance = 'linear';
@@ -159,7 +162,9 @@ end
 
 % Parse
 parse(ip, M, matrixType, varargin{:});
-%%%%%%%%%%%% End input parser %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% End input parser
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Verify the confusion matrix is square
 if size(M, 1) ~= size (M, 2)
