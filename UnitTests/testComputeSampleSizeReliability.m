7ded955b68df302a2e6dd20d67d6c9affa9a190c
Blair Kaneshiro <blairbo@ccrma.stanford.edu>
2019-09-02 18:21:59 -0700

    init testComputeSampleSizeReliability

---
+++ UnitTests/testComputeSampleSizeReliability.m
@@ -0,0 +1,6 @@
+% testComputeSampleSizeReliability.m
+% -------------------------------------------
+% Blair - Sep 02, 2019
+
+clear all; close all; clc
+load('/user/b/blairbo/MatClassRSA/UnitTests/S06.mat')
