042db259f1e5b57b70a82c4ba4f8240a770e560c
Bernard Wang <berneezy@Bernards-MacBook-Pro-2.local>
2019-09-03 17:17:28 -0700

    fixing issues from Nathan testing for classifyPredict and classifyTrain

--- UnitTests/testClassifyPredict.m
+++ UnitTests/testClassifyPredict.m
@@ -9,7 +9,13 @@ clear all; close all; clc
 rng('shuffle');
 
 % S06.mat should be in UnitTests directory
+uiwait(msgbox('In the next window, select the directory containing the .mat file ''S06.mat.'''))
+inDir = uigetdir(pwd);
+currDir = pwd;
+cd(inDir)
 load('S06.mat');
+cd(currDir)
+%%
 [dim1, dim2, dim3] = size(X);
 X_2D = reshape(X, [dim1*dim2, dim3]);
 X_2D = X_2D';
@@ -18,6 +24,13 @@ X_2D = X_2D';
 % Issue related to the random seed:
 %    - Although the confusion matrices are the same for both runs, the
 %      C.predY and C1.predY vectors are different.
+%      ### BERNARD UPDATE:  C.predY and C1.predY should now be equal ###
+
+trainData = X_2D(1:floor(dim3/5), :);
+trainLabels = labels6(1:floor(dim3/5));
+testData = X_2D(floor(dim3/5)+1:dim3, :);
+testLabels = labels6(floor(dim3/5)+1:dim3);
+
 
 M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'LDA', 'PCA', 0, 'randomSeed', 1);
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3), 'randomSeed', 1);
@@ -30,7 +43,9 @@ figure; plotMatrix(C1.CM, 'matrixLabels', 0, 'colorMap', 'jet');
 %% Classification and prediction with 2D data using random forest
 % Runs successfully, and CM looks OK.
 % Issue:
-%    - Why is PCA being run here?
+%    - Why is PCA being run here? 
+%   ### Bernard: PCA is turned to .99 by default ###
+%   
 
 M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'RF', 'numTrees', 200, 'minLeafSize', 1);
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3));
@@ -39,12 +54,12 @@ figure; plotMatrix(C.CM, 'matrixLabels', 0, 'colorMap', 'jet');
 
 %% Classification and prediction with 2D data using SVM
 % Issues:
-%    - Output CM does not make any sense
+%    - Output CM does not make any sense 
 %    - Already stated in testClassifyTrain.m, but even though {'PCA', 0} is
 %      set, PCA is being run.  However, if we do not input the kernel
-%      name-value pair, PCA is not run (which is expected).
+%      name-value pair, PCA is not run (which is expected). (FIXED)
 
-M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'SVM', 'kernel', 'rbf', 'PCA', 0);
+M = classifyTrain( X_2D(1:floor(dim3/5), :) , labels6(1:floor(dim3/5)), 'classifier', 'SVM', 'PCA', 0, 'kernel', 'rbf');
 C = classifyPredict( M, X_2D(floor(dim3/5)+1:dim3, :),  labels6(floor(dim3/5)+1:dim3));
 
 figure; plotMatrix(C.CM, 'matrixLabels', 0, 'colorMap', 'jet');
