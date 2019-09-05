3bf12c39cef1e4ba6500b6a9e325285857d2447b
nathankong <kongnathan@gmail.com>
2019-09-05 14:10:33 -0700

    Illustrative scripts for reliability.

--- UnitTests/testAverageTrials.m
+++ UnitTests/testAverageTrials.m
@@ -76,7 +76,7 @@ end
 % Results in an averaged trial from trials of different labels
 
 Y = randi(10, [1 nTrial]);
-[x3, y3, p3, o3] = averageTrials(X_3D, Y, 5, P, 'endShuffle', 1, 'handleRemainder', 'newGroup');
+[x3, y3, p3, o3] = averageTrials(X_3D, Y, 5, P, 'endShuffle', 0, 'handleRemainder', 'newGroup');
 
 % Test that averaged data are as expected
 assert(length(y3) == length(o3), 'Lengths of output labels and pseudotrials unequal');
