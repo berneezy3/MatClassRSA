5387883c4db8cf387f1b8f5d121d04b5ffd7ef33
Blair Kaneshiro <blairbo@ccrma.stanford.edu>
2019-09-22 16:27:56 -0700

    update for new functionalities

--- UnitTests/testTransformAndScaleMatrix.m
+++ UnitTests/testTransformAndScaleMatrix.m
@@ -1,18 +1,18 @@
-% testComputeClassificationRDM.m
+% testTransformAndScaleMatrix.m
 
 % General checks: Try out different parameter configurations when inputting
 % a confusion-type matrix and a pairwise accuracy-type matrix
 
 % In addition to general checks please try the following conditions:
-% - Check the params output when inputting (1) multiclass and (2) pairwise
+% - Check the params output when inputting (1) distance and (2) similarity
 % matrices with no name-value pairs to make sure they are the correct 
 % defaults for each case
-% - If inputting a pairwise matrix and calling the function with defaults,
-% the output should be the same as the input. (Maybe this doesn't make
-% sense, design-wise? If so we could change the ranking default to compute 
-% ranks or percentile ranks)
-% - Check cases where the user input a pairwise matrix along with not-allowed
-% specifications for normalize and distance name-value pairs.
-% - when specifying pairwise input, try inputting a non-symmetric matrix
+% - Does the 'zeroToOne' normalization make sense for pairwise accuracy
+% matrices?
+% - Check cases where the user input a distance or similarity matrix along 
+% with not-allowed specifications for name-value pairs -- see if the
+% correct warning prints and if the params reflect what the docstring says
+% the function will do in terms of overriding user input
+% - when specifying distance input, try inputting a non-symmetric matrix
 % and asking the function to rank distances, and see if the output is
 % symmetric and ranked based on the lower triangle only
