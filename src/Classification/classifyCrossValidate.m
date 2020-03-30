91e8f09a2e7a5d1d2f4f83efad15219076c4fca4
berneezy3 <bernardcwang@gmail.com>
2020-03-30 12:24:40 -0700

    In the middle of addng permutation testing back in....

--- src/Classification/classifyCrossValidate.m
+++ src/Classification/classifyCrossValidate.m
@@ -70,6 +70,37 @@
 %   128.
 %   'minLeafSize' - Choose the minimum number of observations per tree leaf.
 %   Default is 1,
+%   pValueMethod - Choose the method to compute p-value.  
+%       --options--
+%       'off'
+%           permutation testing is not conducted
+%       'binomcdf' 
+%           computes a binomial cdf at each of the values in x using number 
+%           of permutations sepcified in 'permutations'in N and probability 
+%           of success for each trial in p
+%       'permuteTestLabels' 
+%           with the 'permuteTestLabels' option, we perform k -fold cross 
+%           validation only once. In each fold, the classification model is   
+%           trained on the intact data and labels, but predictions are made
+%           on test observations whose labels have been shuffled. The prediction   
+%           is repeated N times, with the test labels re-randomized for each   
+%           attempt. The 'permuteTestLabels' option is the second fastest method,   
+%           since it requires training the k models only once, but a total of
+%           k * N  prediction operations are performed. So that there are enough
+%           test labels to randomize in each fold, here we also recommend having   
+%           at least 100 observations total, and no more than 10 cross-validation folds.
+%       'permuteFullModel'
+%           With the ?permuteFullModel? option, we perform the entire 10-fold 
+%           cross validation N times. For each of the N permutation iterations, 
+%           the entire labels vector (training and test observations) is shuffled, 
+%           and in each fold, the classifier model is both trained and tested using 
+%           the shuffled labels. As the full classification procedure is performed 
+%           N times, the ?permuteFullModel? option is the slowest, but is suitable 
+%           to use with any classifier configuration, including settings with 
+%           unbalanced classes, few observations, and up to N-fold cross validation.
+%   'permutations' - Choose number of permutations to perform, if permutation 
+%           testing is turned on.  Default 1000.  This option will only work 
+%           if 'permuteFullModel' or 'permuteTestLabels' is chosen.
 %
 % OUTPUT ARGS 
 %   C - output object containing all cross validation related
@@ -436,15 +467,13 @@
         predY = predictionsConcat;
     end
 %}
-    
-%{    
+       
     switch ip.Results.pValueMethod
         case 'permuteFullModel'
             pVal = permTestPVal(accuracy, accDist);
         otherwise
             pVal = '';    
     end
-%}
 
     C.modelsConcat = modelsConcat;
     C.predY = predictionsConcat;
