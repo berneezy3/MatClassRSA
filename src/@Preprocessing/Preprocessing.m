a85ba2e0a221b29bff0ae269b66d680fab6f0c50
berneezy3 <bernardcwang@gmail.com>
2020-07-13 17:37:22 -0700

    added docstrings for MatClassRSA object classes

--- src/@Preprocessing/Preprocessing.m
+++ src/@Preprocessing/Preprocessing.m
@@ -1,4 +1,22 @@
 classdef Preprocessing
+% This class contains all the data preprocessing related functions of
+% MatClassRSA.  Typically, EEG data is first processed by this functions
+% contained in this class, then passed to the "Classification" class to 
+% generate confusion matrices.  
+%
+% To call a preprocessing function, you must first create a instance of
+% the MatClassRSA object, access the Preprocessing member variable, then
+% call the function of interest.  Below is an example call to the function
+% shuffleData():
+%
+% RSA = MatClassRSA;
+% RSA.Preprocessing.shuffleData(X, Y);
+%
+% Below are the list of classification functions contained in this class:
+%   - averageTrials()
+%   - shuffleData()
+%   - noiseNormalization()
+
    properties
    end
    methods
