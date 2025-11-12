e29580b980627a3bc273a5b40a0ca5b44e98180e
Blair Kaneshiro <blairbo@gmail.com>
2025-11-12 13:39:03 -0900

    docstring edits add license

--- ExampleFunctionCalls/example_Preprocessing_averageTrials.m
+++ ExampleFunctionCalls/example_Preprocessing_averageTrials.m
@@ -1,9 +1,31 @@
-% averageTrials.m
-% ---------------------
-% Ray - March, 2025
+% example_Preprocessing_averageTrials.m
+% -------------------------------------
+% Example function calls for averageTrials() function within
+% the +Preprocessing module
+
+% This software is released under the MIT License, as follows:
 %
-% Example function calls for Preprocessing.averageTrials() function within
-% the +PreProcessing module
+% Copyright (c) 2025 Bernard C. Wang, Raymond Gifford, Nathan C. L. Kong, 
+% Feng Ruan, Anthony M. Norcia, and Blair Kaneshiro.
+% 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files (the 
+% "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to 
+% permit persons to whom the Software is furnished to do so, subject to 
+% the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 clear all; close all; clc
--- ExampleFunctionCalls/example_Visualization_plotDendrogram.m
+++ ExampleFunctionCalls/example_Visualization_plotDendrogram.m
@@ -1,9 +1,35 @@
 % example_Visualization_plotDendrogram.m
-% ---------------------
-% Ray - May, 2025
-%
+% ---------------------------------------
 % Example function calls for plotDendrogram() function within
 % the +Visualization module
+%
+% This example requires one or more example data files. Run the 
+% illustrative_0_downloadExampleData script in the IllustrativeAnalyses 
+% folder if you have not already downloaded the example data. 
+
+% This software is released under the MIT License, as follows:
+%
+% Copyright (c) 2025 Bernard C. Wang, Raymond Gifford, Nathan C. L. Kong, 
+% Feng Ruan, Anthony M. Norcia, and Blair Kaneshiro.
+% 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files (the 
+% "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to 
+% permit persons to whom the Software is furnished to do so, subject to 
+% the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 clear all; close all; clc;
 
@@ -44,7 +70,7 @@ catLabels = {'HB', 'HF', 'AB', 'AF', 'FV', 'IO'};
 figure;
 Visualization.plotDendrogram(RDM)
 
-%% Adding Normalziation
+%% Adding Normalization
 
 figure;
 Visualization.plotDendrogram(RDM, 'normalization', true)
@@ -58,7 +84,7 @@ figure;
 Visualization.plotDendrogram(RDM, 'nodeLabels', catLabels, 'nodeColors', rgb6, 'fontSize', 10);
 %% Changing Order of Classes
 
-% specify new order of classes to display 
+% specify new order of classes to display (may introduce crossovers)
 order = [4,5,6,1,2,3];
 
 figure;
